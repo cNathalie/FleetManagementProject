72bf90e25487be0982416ed91c4590f68e2a6613
mereldegendt <merel.degendt@student.hogent.be>
2023-11-08 17:23:53 +0100

    v1

---
+++ frontend/src/constants/listHeader.js
@@ -0,0 +1,15 @@
+const listHeaders = [
+    {
+      eigenaar: "Eigenaar",
+      merk: "Merk",
+      model: "Model",
+      chassisnummer: "Chassisnummer",
+      nummerplaat: "Nummerplaat",
+      brandstoftype: "Brandstoftype",
+      typeWagen: "Type wagen",
+      kleur: "Kleur",
+      aantalDeuren: "Aantal deuren",
+    },
+  ];
+  
+  export default listHeaders;
