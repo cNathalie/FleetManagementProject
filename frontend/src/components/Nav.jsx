5fbb5c3d436530bc96e5d4f9db3c4e4c8a8a3a37
JornDZ <114744832+JornDZ@users.noreply.github.com>
2023-11-21 20:11:49 +0100

    fixen van navbar
    navbar gefixt en in main admin routing verplaatss

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -3,104 +3,128 @@ import React from "react";
 import LogoImg from "./Logo";
 import { useNavigate } from "react-router-dom";
 import Button from "./Button";
+import homePage from "../constants/homePageContent";
+import LogOutButton from "./LogOutButton";
 
 const Nav = () => {
   const navigate = useNavigate();
 
   return (
-    <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600 ">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 ">
-        <a
+    <div className="w-full bg-white fixed border-b border-gray-200 flex flex-wrap items-center justify-between mx-auto">
+      <div className="pt-2 pl-6">
+        <Button
           onClick={() => {
             navigate("/home");
           }}
-          className="flex items-center"
         >
-          <LogoImg />
-        </a>
-        <div className="flex md:order-2 ">
+          <img
+            src="../src/assets/Media/FM_noBG.png"
+            alt="logo"
+            className="w-[151px] h-[60px]"
+          />
+        </Button>
+      </div>
+      <div className=" flex flex-wrap">
+        {homePage.map((h) => {
+          return (
+            <Button
+              key={h.title}
+              onClick={() => {
+                navigate(`/items/${h.id}`);
+              }}
+              className="py-2 pl-3 pr-4 text-blueText rounded hover:bg-gray-100 md:p-0"
+            >
+              {h.title}
+            </Button>
+          );
+        })}
+      </div>
+      <div>
+        <div className="px-6 flex flex-wrap">
           <Button
-            className="text-white bg-blueBtn hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="text-white bg-blueBtn hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center"
             onClick={() => {
               navigate("/admin");
             }}
           >
             Administratie
           </Button>
-          <button
-            data-collapse-toggle="navbar-sticky"
-            type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-sticky"
-            aria-expanded="false"
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          </button>
-        </div>
-        <div
-          className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-          id="navbar-sticky"
-        >
-          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-              <a
-                onClick={() => {
-                  navigate("/items/1");
-                }}
-                className="block py-2 pl-3 pr-4 text-blueText rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blueTextHover md:hover:underline md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Voertuigen
-              </a>
-            </li>
-            <li>
-              <a
-                onClick={() => {
-                  navigate("/items/2");
-                }}
-                className="block py-2 pl-3 pr-4 text-blueText rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blueTextHover md:hover:underline md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Bestuurders
-              </a>
-            </li>
-            <li>
-              <a
-                onClick={() => {
-                  navigate("/items/3");
-                }}
-                className="block py-2 pl-3 pr-4 text-blueText rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blueTextHover md:hover:underline md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Tankkaarten
-              </a>
-            </li>
-            <li>
-              <a
-                onClick={() => {
-                  navigate("/items/4");
-                }}
-                className="block py-2 pl-3 pr-4 text-blueText rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blueTextHover md:hover:underline md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Fleet
-              </a>
-            </li>
-          </ul>
+          <div className="pl-4">
+            <LogOutButton />
+          </div>
         </div>
       </div>
-    </nav>
+    </div>
+    // <div className="bg-black fixed w-full z-20 border-b border-gray-200">
+    //   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    //     <div className="fixed left-0 top-0">
+    //       <Button
+    //         onClick={() => {
+    //           navigate("/home");
+    //         }}
+    //       >
+    //         <LogoImg />
+    //       </Button>
+    //     </div>
+    //     <div className="flex md:order-2 ">
+    //       <Button
+    //         className="text-white bg-blueBtn hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 fixed top-0 right-0"
+    //         onClick={() => {
+    //           navigate("/admin");
+    //         }}
+    //       >
+    //         Administratie
+    //       </Button>
+    //     </div>
+    //     <div
+    //       className="items-center justify-between w-full md:flex md:w-auto md:order-1"
+    //       id="navbar-sticky"
+    //     >
+    //       <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+    //         <li>
+    //           <a
+    //             onClick={() => {
+    //               navigate("/items/1");
+    //             }}
+    //             className="block py-2 pl-3 pr-4 text-blueText rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blueTextHover md:hover:underline md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+    //           >
+    //             Voertuigen
+    //           </a>
+    //         </li>
+    //         <li>
+    //           <a
+    //             onClick={() => {
+    //               navigate("/items/2");
+    //             }}
+    //             className="block py-2 pl-3 pr-4 text-blueText rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blueTextHover md:hover:underline md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+    //           >
+    //             Bestuurders
+    //           </a>
+    //         </li>
+    //         <li>
+    //           <a
+    //             onClick={() => {
+    //               navigate("/items/3");
+    //             }}
+    //             className="block py-2 pl-3 pr-4 text-blueText rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blueTextHover md:hover:underline md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+    //           >
+    //             Tankkaarten
+    //           </a>
+    //         </li>
+    //         <li>
+    //           <a
+    //             onClick={() => {
+    //               navigate("/items/4");
+    //             }}
+    //             className="block py-2 pl-3 pr-4 text-blueText rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blueTextHover md:hover:underline md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+    //           >
+    //             Fleet
+    //           </a>
+    //         </li>
+    //       </ul>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
