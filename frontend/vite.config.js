f1e42d216cae50b8ff52957628fda74cf30d241f
cNathalie <nathalie.courteaux@student.hogent.be>
2024-01-04 19:12:09 +0100

    Dockerfile frontend ok
    kan aparte werkende container doen draaien
    
    vite.config.js past zich dynamisch aan voor docker omgeving

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const isDocker = process.env.VITE_DOCKER === "true";
+
+const dockerHost = {
+  plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+  },
+  host: true
+};
+
+const localHost = {
+  plugins: [react()],
+  server: {
+    port: 5173,
+  },
+};
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(isDocker ? dockerHost : localHost);
